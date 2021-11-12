8588df1efb4e668e44c74ae0bd7fd64d49aaacea
Morenike Oyewole <morenikeoyewole@Morenikes-MacBook-Pro.local>
2021-11-12 09:13:09 +0100

    updates

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,24 +1,24 @@
+// const ctx = document.getElementById("chart").getContext('2d');
+//       const myChart = new Chart(ctx, {
+//         type: 'line',
+//         data: {
+//           labels: [ "Mon", "Tue",
+//           "Wed", "Thu", "Fri", "Sat", "Sun"],
+//           datasets: [{
+//             label: 'Last week',
+//             backgroundColor: 'rgba(161, 198, 247, 1)',
+//             borderColor: 'rgb(47, 128, 237)',
+//             data: [3000, 7000, 5000, 3000, 9000, 2000, 3000, 4000, 2000, 5000, 8000, 3000, 7000, 5000, 3000, 9000, 2000],
+//           }]
+//         },
+//         options: {
+//           scales: {
+//             yAxes: [{
+//               ticks: {
+//                 beginAtZero: true,
+//               }
+//             }]
+//           }
+//         },
+//       });a
 "use strict";
-
-var ctx = document.getElementById("chart").getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"],
-    datasets: [{
-      label: 'Last week',
-      backgroundColor: 'rgba(161, 198, 247, 1)',
-      borderColor: 'rgb(47, 128, 237)',
-      data: [3000, 4000, 2000, 5000, 8000, 9000, 2000]
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
-});
